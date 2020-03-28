f1de3bb1bc46f04b20066ce6b07f10bb99e19fde
Ngo Van Tuan <contact.tuannv@gmail.com>
2020-03-29 03:05:45 +0700

    Add project files.

---
+++ Students Management/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Students_Management
+{
+    class Student : IStudent
+    {
+        public Student()
+        {
+        }
+
+        public Student(string fullName, string dateofBirth, string native, string @class, string phoneNo, int mobile)
+        {
+            FullName = fullName;
+            ID = Program.students.Count;
+            DateofBirth = dateofBirth;
+            Native = native;
+            Class = @class;
+            PhoneNo = phoneNo;
+            Mobile = mobile;
+        }
+
+        public string FullName { get; set; }
+
+        public int ID { get; set; }
+
+        public string DateofBirth { get; set; }
+
+        public string Native { get; set; }
+
+        public string Class { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public int Mobile { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"Full name: {FullName} - ID: {ID} - Date of birth: {DateofBirth} - Native: {Native} - Class: {Class} - Phone no: {PhoneNo} - Mobile: {Mobile}");
+        }
+    }
+}
